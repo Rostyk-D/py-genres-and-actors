bdf30c1d04d5015bcc216459805a67c2e0e67947
Rostyk <rostik.durjagin@gmail.com>
2026-04-14 14:53:05 +0300

    fix some problem

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ from db.models import Actor, Genre
 def main() -> QuerySet:
     # --- CREATE ---
 
-    Genre.objects.create(name="Western")
-    Genre.objects.create(name="Action")
-    Genre.objects.create(name="Dramma")
+    genres = [("Western",), ("Action",), ("Dramma",)]
+    for (name,) in genres:
+        Genre.objects.create(name=name)
 
-    Actor.objects.create(first_name="George", last_name="Klooney")
-    Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
+    actors = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
+    ]
+    for first_name, last_name in actors:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
 
     # --- UPDATE ---
 
@@ -31,7 +35,6 @@ def main() -> QuerySet:
     # --- DELETE ---
 
     Genre.objects.filter(name="Action").delete()
-
     Actor.objects.filter(first_name="Scarlett").delete()
 
     # --- RETURN ---
